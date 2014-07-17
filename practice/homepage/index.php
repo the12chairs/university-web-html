ba4952db7924b96124a52d6c5fa0d796e3a1e5d4
Maksim Bondarenko <mbondarenko92@gmail.com>
2014-07-17 18:19:09 +0700

    Work at tree

---
+++ practice/homepage/index.php
@@ -0,0 +1,36 @@
+<?php @include_once('../settings.php'); ?>
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+    </head>
+    <body>
+        <section>
+            <div id="header">
+                <?php @include_once("../layout/header/content.php"); ?>
+            </div>
+        </section>
+        <section>
+            <div id="gray_block">
+                <?php @include_once("../layout/gray_block/content.php"); ?>
+            </div>
+        </section>
+        <section>
+            <div id="blue_block">
+                <?php @include_once("../layout/blue_block/content.php"); ?>
+            </div>
+        </section>
+        <section>
+            <div id="gallery">
+                <?php @include_once("../layout/gallery/content.php"); ?>
+            </div>
+        </section>
+        <section>
+            <div id="links">
+                <?php @include_once("../layout/links/content.php"); ?>
+            </div>
+        </section>
+
+    </body>
+</html>
